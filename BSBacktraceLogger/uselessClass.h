1596c9a2a059bbcca0ef388137d647c8882109f5
Huang, Kiros <kiros.huang@sap.com>
2020-06-05 15:38:43 +0800

    add uselessClass for OClint test

---
+++ BSBacktraceLogger/uselessClass.h
@@ -0,0 +1,17 @@
+//
+//  uselessClass.h
+//  BSBacktraceLogger
+//
+//  Created by Huang, Kiros on 2020/6/5.
+//  Copyright © 2020 bestswifter. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface uselessClass : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ BSBacktraceLogger/uselessClass.m
@@ -0,0 +1,13 @@
+//
+//  uselessClass.m
+//  BSBacktraceLogger
+//
+//  Created by Huang, Kiros on 2020/6/5.
+//  Copyright © 2020 bestswifter. All rights reserved.
+//
+
+#import "uselessClass.h"
+
+@implementation uselessClass
+
+@end
